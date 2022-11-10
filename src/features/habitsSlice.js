61bbfee4be3cc25c40329b96db46291f2544b1f7
sadaab2784 <ali.sadaab825313@gmail.com>
2022-11-10 10:46:58 +0530

    Upadated

--- src/features/habitsSlice.js
+++ src/features/habitsSlice.js
@@ -1,58 +1,59 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 export const details = [
-  { day: 'Mon', status: 'none' },
-  { day: 'Tue', status: 'none' },
-  { day: 'Wed', status: 'none' },
-  { day: 'Thu', status: 'none' },
-  { day: 'Fri', status: 'none' },
-  { day: 'Sat', status: 'none' },
-  { day: 'Sun', status: 'none' },
-]
+  { day: "Mon", status: "none" },
+  { day: "Tue", status: "none" },
+  { day: "Wed", status: "none" },
+  { day: "Thu", status: "none" },
+  { day: "Fri", status: "none" },
+  { day: "Sat", status: "none" },
+  { day: "Sun", status: "none" },
+];
 
 // load habits from local storage if there are any
-const habitsFromStorage = localStorage.getItem('newHabits')
-  ? JSON.parse(localStorage.getItem('newHabits'))
-  : [{ title: 'early bird', description: 'wake up at 3:45am', details }]
+const habitsFromStorage = localStorage.getItem("newHabits")
+  ? JSON.parse(localStorage.getItem("newHabits"))
+  : [{ title: "Wake Up", description: "wake up at 3:45am", details }];
 
-let habits = [...habitsFromStorage]
+let habits = [...habitsFromStorage];
 
 const initialState = {
   habits: habits,
-}
-
+};
 
 // habitsSlice contains store, actions and reducers
 const habitsSlice = createSlice({
-  name: 'habits',
+  name: "habits",
   initialState,
   reducers: {
     addHabit: (state, { payload }) => {
-      state.habits = [...state.habits, payload]
-      habits = [...habits, payload]
-      window.localStorage.setItem('newHabits', JSON.stringify(habits))
+      state.habits = [...state.habits, payload];
+      habits = [...habits, payload];
+      window.localStorage.setItem("newHabits", JSON.stringify(habits));
     },
     deleteHabit: (state, action) => {
-      state.habits = state.habits.filter((habit) => habit.title !== action.payload)
-      window.localStorage.setItem('newHabits', JSON.stringify(state.habits))
+      state.habits = state.habits.filter(
+        (habit) => habit.title !== action.payload
+      );
+      window.localStorage.setItem("newHabits", JSON.stringify(state.habits));
     },
     changeStatus: (state, { payload }) => {
       state.habits.forEach((habit) => {
         if (habit.title === payload.title) {
           habit.details.forEach((detail) => {
             if (detail.day === payload.details[0].day) {
-              detail.status = payload.details[0].status
+              detail.status = payload.details[0].status;
             }
-          })
+          });
         }
-      })
-      window.localStorage.setItem('newHabits', JSON.stringify(state.habits))
+      });
+      window.localStorage.setItem("newHabits", JSON.stringify(state.habits));
     },
   },
-})
+});
 
-console.log(habitsSlice)
+console.log(habitsSlice);
 
-export const { addHabit, deleteHabit, changeStatus } = habitsSlice.actions
+export const { addHabit, deleteHabit, changeStatus } = habitsSlice.actions;
 
-export default habitsSlice.reducer
+export default habitsSlice.reducer;
